59c02b11f5c768840cf51fe8dacb3d7792ebf765
Aryanmehta21 <aryanmehta310@gmail.com>
2023-02-02 11:36:15 +0530

    best time buy and sell stock 2

---
+++ BestStock.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class BestStock {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maxProfit(arr));
+        sc.close();
+    }
+
+    public static int maxProfit(int[] prices){
+        int profit = 0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i] > prices[i-1]){
+                profit += (prices[i] - prices[i-1]);
+            }
+        }
+        return profit;
+    }
+}
