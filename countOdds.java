5384f0ec47551c6aceaae0d123a1624ed5c91b2b
Aryanmehta21 <aryanmehta310@gmail.com>
2023-02-13 16:15:34 +0530

    13-02-2023

--- countOdds.java
+++ countOdds.java
@@ -11,3 +11,23 @@ class Solution {
         return ans;
     }
 }
+
+// Optimised all testcases passed.
+class Solution {
+    public int countOdds(int low, int high) {
+        // int ans = 0;
+        // for(int i=low;i<=high;i++){
+        //     if(i%2 ==1){
+        //         ans++;
+        //     }
+        // }
+        // return ans;
+        int total = high-low+1;
+        if(low%2!=0 && high%2!=0){
+            return total/2+1;
+        }else{
+            return total/2;
+        }  
+
+    }
+}
