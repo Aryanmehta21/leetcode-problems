e639db4758033668c9df4b650791fb24247c6b43
Aryanmehta21 <aryanmehta310@gmail.com>
2023-02-13 11:54:44 +0530

    13-02-2023

---
+++ countOdds.java
@@ -0,0 +1,13 @@
+
+// Time Limit Exceed on Leetcode.
+class Solution {
+    public int countOdds(int low, int high) {
+        int ans = 0;
+        for(int i=low;i<=high;i++){
+            if(i%2 ==1){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
