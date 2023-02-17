0741a4755188a3fa19ff473ec674db5d9bfb6827
Aryanmehta21 <aryanmehta310@gmail.com>
2023-02-18 01:10:50 +0530

    17-02-2023

---
+++ minDiffInBST.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDiffInBST(TreeNode root) {
+        
+    if(root==null){
+            return 0;
+        }
+        ArrayList<Integer>list=new ArrayList<>();
+        inOrderTraversal(root,list);
+
+        int min=Integer.MAX_VALUE;
+
+        for(int i=0;i<list.size()-1;i++){
+            min= Math.min(min,list.get(i+1)-list.get(i));
+        }
+        return min;
+    }
+    public static void inOrderTraversal(TreeNode root,ArrayList<Integer>list){
+
+        if(root==null){
+            return;
+        }
+
+        inOrderTraversal(root.left,list);
+        list.add(root.val);
+        inOrderTraversal(root.right,list);
+    }
+}
