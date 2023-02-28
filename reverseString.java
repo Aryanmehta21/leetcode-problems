ea8ffea9242f8226d1976013edc6becb372934ba
Aryanmehta21 <aryanmehta310@gmail.com>
2023-03-01 01:22:21 +0530

    01-03-2023

---
+++ reverseString.java
@@ -0,0 +1,16 @@
+// Leetcode - 344
+
+class Solution {
+    public void reverseString(char[] s) {
+        int startIndex = 0;
+        int endIndex = s.length - 1;
+        char temp;
+        while (startIndex < endIndex) {
+            temp = s[startIndex];
+            s[startIndex] = s[endIndex];
+            s[endIndex] = temp;
+            startIndex++;
+            endIndex--;
+            }
+        }
+    }
