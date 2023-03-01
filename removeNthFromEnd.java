fd79c635c00fcaee14f85819673c7ff0a768c20e
Aryanmehta21 <aryanmehta310@gmail.com>
2023-03-02 00:48:52 +0530

    02-03-2023

---
+++ removeNthFromEnd.java
@@ -0,0 +1,43 @@
+// Leetcode - 19
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // ListNode slow = head;
+        // ListNode fast = head;
+        // for(int i =0;i<n;i++){
+        //     fast = fast.next;
+        // }
+        // while(fast!=null&&fast.next!=null){
+        //     slow = slow.next;
+        //     fast = fast.next;
+        // }
+        // slow.next = slow.next.next;
+        // return slow.next;
+    ListNode start = new ListNode(0);
+    ListNode slow = start, fast = start;
+    start.next = head;
+    
+    
+    for(int i=1; i<=n+1; i++)   {
+        fast = fast.next;
+    }
+    
+    while(fast != null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    slow.next = slow.next.next;
+    return start.next;
+    }
+}
