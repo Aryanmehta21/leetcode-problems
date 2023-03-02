093a085701eb6be94d175f00ed05fae1693a3104
Aryanmehta21 <aryanmehta310@gmail.com>
2023-03-03 01:23:40 +0530

    03-03-2023

---
+++ lengthOfLongestSubstring.java
@@ -0,0 +1,24 @@
+// Leetcode - 3
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+       int max=0;
+        HashSet<Character>hash=new HashSet<Character>();
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<s.length())
+        {
+            if(hash.contains(s.charAt(j)))
+            {
+                hash.remove(s.charAt(i));
+                i=i+1;
+            }
+            else
+            {
+             hash.add(s.charAt(j));
+             j=j+1;
+             max=Math.max(j-i,max);   
+            }
+        }
+        return max;  
+    }
+}
