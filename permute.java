0644d38888ccb582d8f1d2f3d4e8a4ba315b4954
Aryanmehta21 <aryanmehta310@gmail.com>
2023-03-08 01:58:30 +0530

    08-03-2023

--- permute.java
+++ permute.java
@@ -7,28 +7,29 @@ class Solution {
         return ans;
     }
 
-    void helper(int pos, int n, int[] nums, List<List<Integer>> ans){
-        if(pos>=n){
+    void helper(int pos, int n, int[] nums, List<List<Integer>> ans) {
+        if (pos >= n) {
             List<Integer> permutation = convertToArray(nums);
             ans.add(new ArrayList(permutation));
             return;
         }
-        for(int i = pos;i<n;i++){
+        for (int i = pos; i < n; i++) {
             swap(pos, i, nums);
-            helper(pos+1, n, nums, ans);
-            swap(pos,i,nums);
+            helper(pos + 1, n, nums, ans);
+            swap(pos, i, nums);
         }
         return;
     }
-    void swap(int i,int j, int[] nums){
+
+    void swap(int i, int j, int[] nums) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    List<Integer> convertToArray(int[] nums){
+    List<Integer> convertToArray(int[] nums) {
         List<Integer> ans = new ArrayList<>();
-        for(int ele:nums){
+        for (int ele : nums) {
             ans.add(ele);
         }
         return ans;
