bf44928e0322624ba51d7f44842c231659beecb6
Aryanmehta21 <aryanmehta310@gmail.com>
2023-03-15 01:58:23 +0530

    15-03-2023

---
+++ isSymmetric.java
@@ -0,0 +1,36 @@
+// Leetcode - 101
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+
+        return isTrue(root.left, root.right);
+    }
+
+    boolean isTrue(TreeNode node1, TreeNode node2){
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+
+        return node1.val == node2.val && isTrue(node1.left, node2.right) && isTrue(node2.left, node1.right);
+    }
+}
