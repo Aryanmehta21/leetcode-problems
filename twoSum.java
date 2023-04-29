92410d9b33f65ca8c6f1cc542868e3de6b890a3b
Aryanmehta21 <aryanmehta310@gmail.com>
2023-04-29 14:51:25 +0530

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -19,8 +19,6 @@ class Solution {
 
 
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] nums = new int[n];
+       
         
 }
