1329519fac60e317ea03f07c9efe1c059c76329a
Aryanmehta21 <aryanmehta310@gmail.com>
2023-05-07 14:08:58 +0530

    Create allCodes.java

---
+++ allCodes.java
@@ -0,0 +1,19 @@
+public class  {
+    
+}
+class allCodes {
+    public boolean hasAllCodes(String s, int k) {
+      int count =0;
+      int start = 0;
+      Set seen = new HashSet();
+      for(int i =0;i<s.length();i++){
+          count++;
+          if(count==k){
+              seen.add(s.substring(start, start+k));
+              start++;
+              count--;
+          }
+      }  
+      return seen.size() == Math.pow(2,k);
+    }
+}
