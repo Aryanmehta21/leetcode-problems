aaae5cbabe920c892d19229c2657e3b47e42b78f
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-18 10:32:56 +0530

    kadanes algo

---
+++ MaximumSumSubarray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class MaximumSumSubarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maxSubarray(arr));
+    }
+    public static int maxSubarray(int[] nums){
+        int maxSum = nums[0];
+        int curSum = 0;
+        for(int i =0;i<nums.length;i++){
+            curSum = curSum + nums[i];
+            if(curSum > maxSum){
+                maxSum = curSum;
+            }
+            if(curSum < 0){
+                curSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
