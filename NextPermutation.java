06d900b19f5303c0e44dc3d2f261c4f433321412
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-18 10:19:04 +0530

    permute function

---
+++ NextPermutation.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class NextPermutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        permutation(arr);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void permutation(int[] nums){
+        if(nums == null || nums.length<2){
+            return;
+        }
+        int i = nums.length-2;
+        while(i>=0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j = nums.length-1;
+            while(j>i && nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i+1, nums.length-1);
+    }
+}
