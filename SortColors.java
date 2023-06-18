630c769960832692c6531559062f681a9a591d68
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-18 10:42:19 +0530

    sort colors

--- SortColors.java
+++ SortColors.java
@@ -11,7 +11,32 @@ public class SortColors {
         for(int i =0;i<n;i++){
             System.out.print(arr[i] + " ");
         }
+        sc.close();
     }
+    public static void sort(int[] nums){
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        
+        for(int num: nums){
+            if(num == 0){
+                red++;
+            }else if(num == 1){
+                white++;
+            }else if(num == 2){
+                blue++;
+            }
+        }
 
-    
+        for(int i=0;i<red;i++){
+            nums[i] = 0;
+        }
+        for(int i = red;i<red+white;i++){
+            nums[i] = 1;
+
+        }
+        for(int i = red+white;i<red+white+blue;i++){
+            nums[i] = 2;
+        }
+    }
 }
