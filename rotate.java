27d12426540d12a229622d306d4623c867319ee8
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-18 11:11:04 +0530

    rotate array

--- rotate.java
+++ rotate.java
@@ -1,7 +1,9 @@
 // Leetcode - 189
 
-class Solution {
-    public void rotate(int[] arr, int k) {
+import java.util.Scanner;
+
+class rotate {
+    public static void rotateA(int[] arr, int k) {
         k=k%arr.length;
         if(k<0){
             k+=arr.length;
@@ -20,5 +22,20 @@ class Solution {
             li++;
             ri--;
         }  
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        rotateA(arr, k);
+        for(int i: arr){
+            System.out.print(i + " ");
+        }
+        sc.close();
     }
 }
