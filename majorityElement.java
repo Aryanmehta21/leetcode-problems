f0da61d5382d4479545014eb66dff357348d21ee
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-21 22:55:56 +0530

    .

--- majorityElement.java
+++ majorityElement.java
@@ -1,19 +1,9 @@
 //  Leetcode - 169
+import java.util.*;
+public class MajorityElement {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-class Solution {
-    public int majorityElement(int[] nums) {
-    int count = 0;
-    int candidate = -1;
-    for (int num : nums) {
-        if (count == 0) {
-            candidate = num;
-            count = 1;
-        } else if (candidate == num) {
-            count++;
-        } else {
-            count--;
-        }
-    }
-    return candidate;
     }
 }
