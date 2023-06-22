ff91cac486a3d214a6e5f5aaebb2d8e2568dce16
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-22 19:32:47 +0530

    LongestSubstring

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -1,21 +1,12 @@
 // Leetcode - 3
 import java.util.*;
 public class LongestSubstring {
-    public int lengthOfLongestSubstring(String s) {
-        int max = 0;
-        HashSet<Character> hash = new HashSet<Character>();
-        int i = 0;
-        int j = 0;
-        while (i < s.length() && j < s.length()) {
-            if (hash.contains(s.charAt(j))) {
-                hash.remove(s.charAt(i));
-                i = i + 1;
-            } else {
-                hash.add(s.charAt(j));
-                j = j + 1;
-                max = Math.max(j - i, max);
-            }
-        }
-        return max;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(length(str));
+    }
+    public static int length(String s){
+        
     }
 }
