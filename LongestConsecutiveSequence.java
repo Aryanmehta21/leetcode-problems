b0bd54a81a382dfb33d920e5e20c528e4c63ae78
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-22 19:05:42 +0530

    .

--- LongestConsecutiveSequence.java
+++ LongestConsecutiveSequence.java
@@ -10,5 +10,26 @@ public class LongestConsecutiveSequence {
 
       System.out.println(longestSeq(arr));
     }
-    
+    public static int longestSeq(int[] nums){
+        if(nums.length == 0){
+            return 0;
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        int maxLength = 1;
+        for(int num:nums){
+            if(!set.contains(num-1)){
+                int currentNum = num;
+                int currentLength = 1;
+                while(set.contains(num+1)){
+                    currentNum = currentNum + 1;
+                    currentLength++;
+                }
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+        return maxLength;
+    }
 }
