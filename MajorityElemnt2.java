9829123158f97b0b697483621ccdd43476c4bea8
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-22 09:22:16 +0530

    Majority Element 2

---
+++ MajorityElemnt2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MajorityElemnt2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i =0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+    }
+}
