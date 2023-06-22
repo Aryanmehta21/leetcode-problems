de421e616b255448d2adbc65dd562942075412c1
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-22 18:16:40 +0530

    Two Sum

--- twoSum.java
+++ twoSum.java
@@ -15,18 +15,13 @@ public class TwoSum {
         }
         sc.close();
     }
-    public static int[] twoSum(int[] nums, int target) {
-    int[] result = new int[2];
-    HashMap<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-        int ans = target - nums[i];
-        if (map.containsKey(ans)) {
-            result[0] = map.get(ans);
-            result[1] = i;
-            return result;
+    
+    public static int[] twoSum(int[] nums, int target){
+        int[] result = new int[2];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            int ans = target- nums[i];
         }
-        map.put(nums[i], i);
     }
-    return result;
-    }        
-}
+} 
