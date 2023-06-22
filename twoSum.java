02a37e086f0e611e6cf4767d6a857f9f82f7ea13
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-22 10:24:39 +0530

    Two Sum

--- twoSum.java
+++ twoSum.java
@@ -1,6 +1,19 @@
 // Leetcode - 1
 import java.util.*;
 public class TwoSum {
+    public static void main(String[] args){
+        Scanner sc = new SCanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int[] ans = twoSum(arr, target);
+        for(int i =0;i<2;i++){
+            System.out.println(ans[i] + " ");
+        }
+    }
     public static int[] twoSum(int[] nums, int target) {
     int[] result = new int[2];
     HashMap<Integer, Integer> map = new HashMap<>();
