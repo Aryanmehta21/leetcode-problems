6bb4613b1cc96a8eb1b8c1143521abe28eed49fb
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-23 00:01:28 +0530

    .

--- BuySellStock.java
+++ BuySellStock.java
@@ -11,17 +11,29 @@ public class BuySellStock {
         sc.close();
     }
 
+    // public static int maxProfit(int[] prices){
+    //     int minPrice = Integer.MAX_VALUE;
+    //     int maxProfit = 0;
+    //     for(int price: prices){
+    //         if(price<minPrice){
+    //             minPrice = price;
+    //         }else{
+    //             maxProfit = Math.max(maxProfit, price-minPrice);
+    //         }
+    //     }
+
+    //     return maxProfit;
+    // }
     public static int maxProfit(int[] prices){
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-        for(int price: prices){
+        for(int price:prices){
             if(price<minPrice){
                 minPrice = price;
             }else{
                 maxProfit = Math.max(maxProfit, price-minPrice);
             }
         }
-
         return maxProfit;
     }
 }
