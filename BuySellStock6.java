00e88be5fe4b7d9fc44b334b32401946af883449
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-24 02:27:03 +0530

    Best Time to buy and sell stock - k transactions allowed

--- BuySellStock6.java
+++ BuySellStock6.java
@@ -11,4 +11,16 @@ public class BuySellStock6 {
         System.out.println(maxProfit(k, arr));
         sc.close();
     }
+
+    public static int maxProfit(int k, int[] prices){
+        int[][] dp = new int[k+1][prices.length];
+        for(int i = 1;i<=k;i++){
+            int max = dp[i-1][0] - prices[0];
+            for(int j = 1;j<prices.length;j++){
+                dp[i][j] = Math.max(dp[i][j-1], prices[j] + max);
+                max = Math.max(max,dp[i-1][j]-prices[j]);
+            }
+        }
+        return dp[k][prices.length-1];
+    }
 }
