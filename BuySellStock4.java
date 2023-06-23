85ab6bfc6ba784342dc53e8825aeb9534eaa74a5
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-24 01:42:02 +0530

    .

--- BuySellStock4.java
+++ BuySellStock4.java
@@ -10,39 +10,39 @@ public class BuySellStock4 {
         System.out.println(profit(arr));
         sc.close();
     }
+    // Pepcoding... (long)
+    // public static int profit(int[] prices){
+    //     int obsp = -prices[0];
+    //     int ossp = 0;
+    //     int ocsp = 0;
 
-    public static int profit(int[] prices){
-        int obsp = -prices[0];
-        int ossp = 0;
-        int ocsp = 0;
+    //     for(int i =1;i<prices.length;i++){
+    //         int nbsp = 0;
+    //         int nssp = 0;
+    //         int ncsp = 0;
 
-        for(int i =1;i<prices.length;i++){
-            int nbsp = 0;
-            int nssp = 0;
-            int ncsp = 0;
+    //         if(ocsp - prices[i] > obsp){
+    //             nbsp = ocsp - prices[i];
+    //         }else{
+    //             nbsp = obsp;
+    //         }
 
-            if(ocsp - prices[i] > obsp){
-                nbsp = ocsp - prices[i];
-            }else{
-                nbsp = obsp;
-            }
+    //         if(obsp + prices[i] > ossp){
+    //             nssp = obsp + prices[i];
+    //         }else{
+    //             nssp = ossp;
+    //         }
 
-            if(obsp + prices[i] > ossp){
-                nssp = obsp + prices[i];
-            }else{
-                nssp = ossp;
-            }
+    //         if(ossp > ocsp){
+    //             ncsp = ossp;
+    //         }else{
+    //             ncsp = ocsp;
+    //         }
 
-            if(ossp > ocsp){
-                ncsp = ossp;
-            }else{
-                ncsp = ocsp;
-            }
-
-            obsp = nbsp;
-            ossp = nssp;
-            ocsp = ncsp;
-        }
-        return ossp;
-    }
+    //         obsp = nbsp;
+    //         ossp = nssp;
+    //         ocsp = ncsp;
+    //     }
+    //     return ossp;
+    // }
 }
