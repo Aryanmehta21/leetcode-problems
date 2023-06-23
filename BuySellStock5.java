1e9f7f8600ea4051bbfac224bdb2d6f224c06dc2
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-24 02:06:33 +0530

    .

--- BuySellStock5.java
+++ BuySellStock5.java
@@ -1,5 +1,11 @@
+import java.util.*;
 public class BuySellStock5 {
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
     }
 }
