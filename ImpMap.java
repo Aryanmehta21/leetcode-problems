ba29cea954efd9344e65f7b39e2b6ffefbf89c69
Aryanmehta21 <aryanmehta310@gmail.com>
2023-06-24 18:45:53 +0530

    .

--- ImpMap.java
+++ ImpMap.java
@@ -1,7 +1,20 @@
-import java.util.HashMap;
+import java.util.*;
 
-public class ImpMap {
-    public static void main(String[] args) {
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int q = sc.nextInt();
+        int[][] queries = new int[q][2];
+        for(int i = 0;i<queries.length;i++){
+            queries[i][0] = sc.nextInt();
+            queries[i][1] = sc.nextInt();
+        }
+
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0;i<arr.length;i++){
             if(map.containsKey(arr[i])){
@@ -10,5 +23,19 @@ public class ImpMap {
                 map.put(arr[i], 1);
             }
         }
+        int sum = 0;
+        for(int i = 0;i<queries.length;i++){
+            int low = queries[i][0];
+            int high = queries[i][1];
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                int frequency = entry.getValue();
+                if (frequency >= low && frequency <= high) {
+                    sum += entry.getKey() * frequency;
+                }
+            }
+            System.out.println(sum);
+            sum = 0; 
+        }
+
     }
 }
