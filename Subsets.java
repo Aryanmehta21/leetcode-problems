f2e6ba525f6c246b1568d5932509284c354332e8
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-06 23:46:45 +0530

    .

--- Subsets.java
+++ Subsets.java
@@ -1,7 +1,14 @@
 import java.util.*;
 public class Subsets {
 
-    public static List<List<Integer>> 
+    public static List<List<Integer>> subsets(int[] arr){
+        List<List<Integer>> res = new ArrayList<>();
+        generateSubsets(0, arr, new ArrayList<>(), res);
+        return res;
+    }
+
+    
+
     public static void main(String[] args){
         Scanner sc  = new Scanner(System.in);
         int n = sc.nextInt();
