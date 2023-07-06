d2a679e7e3419128b28c2f2a14491866bbd9eb26
Aryanmehta21 <aryanmehta310@gmail.com>
2023-07-06 23:44:02 +0530

    .

--- Subsets.java
+++ Subsets.java
@@ -7,6 +7,7 @@ public class Subsets {
         for(int i = 0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        
+        List<Integer> ans = subsets(arr);
+        System.out.println(ans);
     }
 }
